9b6b0a602192db76970c49072e8ff85980da5fc4
yvicenci <yvicenci@our.ecu.edu.au>
2019-10-13 21:54:55 +0800

    Finished GetNumbers.sh

--- portfolio/week 6/GetNumbers.sh
+++ portfolio/week 6/GetNumbers.sh
@@ -11,4 +11,13 @@ getNumber()
 {
     read -p "$1: "
     while (( $REPLY < $2 || $REPLY > $3 )); do
+        printError "Input must be between $2 and $3"
+        read -p "$1: "
+    done
 }
+
+echo "this is the start of the script"
+getNumber "please type a number between 1 and 10" 1 10
+echo "Thank you!"
+getNumber "please type a number between 50 and 100" 50 100
+echo "Thank you!"
