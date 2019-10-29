ba95068ff694323cd46881aa081de060ed17cf70
yvicenci <yvicenci@our.ecu.edu.au>
2019-10-29 14:45:49 +0000

    workshop-6: added Triangles.sh calculates triangle area

---
+++ portfolio/week 6/Triangles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+calculateTriangleArea()
+{
+    local base=$1
+    local height=$2
+    local triangleArea=$(( ($base*$height)/2 ))
+    echo $triangleArea
+}
+
+# main
+for((base=1; base<=10; base++))
+do
+    for((height=1; height<=10; height++))
+    do
+        echo 'The are for a triangle with base ' $base 'and height ' $height 'is 'calculateTriangleArea $base $height
+    done
+done
+exit 0
