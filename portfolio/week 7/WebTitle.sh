4054e5538d0531f785ff3bdad98511e1acedfc5d
yvicenci <yvicenci@our.ecu.edu.au>
2019-11-01 16:46:39 +0000

    For workshop-7 finished

--- portfolio/week 7/WebTitle.sh
+++ portfolio/week 7/WebTitle.sh
@@ -4,11 +4,11 @@ IFS=
 # call Downloader.sh from workshop 6
 ./../week\ 6/Downloader.sh
 
-# extract title of webpage downloaded
-webpage=$(cat index.html)
+# extract title of webContent downloaded
+webContent=$(cat index.html)
 
-webTitle=$(echo "$webpage" | sed -n -e '/<title>/ p' )
+webTitle=$(echo "$webContent" | sed -rn "s@<title>(.*)</title>@\1@p")
 
 # format output
-echo -e "Web page is $webTitle"
+echo "Downloaded page with the title $webTitle"
 exit 0
