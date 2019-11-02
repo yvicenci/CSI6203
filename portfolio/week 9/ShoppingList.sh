c9c80a230898afef332acdf9afc8c1e20a483037
yvicenci <yvicenci@our.ecu.edu.au>
2019-11-02 03:56:39 +0000

    workshop-8: task 4 awk calculations complete

--- portfolio/week 9/ShoppingList.sh
+++ portfolio/week 9/ShoppingList.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# prints and formats the shopping list items, quantity and price into
+# a nice readable format from shopping.csv
+# by Yvonne Vicencio
 
+# message and heading
 echo "Your shopping list is as follows:"
 echo -e "Item\t\t | Quantity | Price "
 echo "-----------------|----------|---------"
-# shopping list format
+
+# shopping list format using awk
 awk 'BEGIN { FS=","; currency="$"}
     { printf "%16s | %2d       | %c%1.2f\t\n", $1, $2, currency, $3 }' ./shopping.csv
