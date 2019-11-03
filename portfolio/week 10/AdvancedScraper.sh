19de670628b465d2f6adcb46cc930da6366e2ace
yvicenci <yvicenci@our.ecu.edu.au>
2019-11-03 18:02:39 +0000

    progress

--- portfolio/week 10/AdvancedScraper.sh
+++ portfolio/week 10/AdvancedScraper.sh
@@ -7,15 +7,22 @@ ecu=$(curl -s https://www.ecu.edu.au/degrees/study-areas/science)
 
 echo "$ecu" | awk '
 BEGIN { startProcess=0 }
-/section id="disciplines"/{
-    startProcess=1
-}
 {
-    while(startProcess==1){
-    next
-    /<a href="https:\/\/www.ecu.edu.au\/degrees\/study-areas\/science\/.* title/  print $0 }
-    /<\/section>/ { startProcess=0 }
+    regexStart="<section id=\"disciplines\".*"
+    if( $0 ~ regexStart ){
+        startProcess=1
+        regexScrape="<a href=\"https://www.ecu.edu.au/degrees/study-areas/science/.*\""
+        regexEnd="</section>"
+        print $0
+        while(startProcess==1){
+            if( $0 ~ regexScrape ){
+                print $0
+        }
+        else if( $0 ~ regexEnd ){
+            startProcess=0
+        }
     }
-}
+    }
 
+}
 ' #| sed -rn "s@<a href=\"https://www.ecu.edu.au/degrees/study-areas/science.*\" title=\".*\">(.*)</a>@\1@p"
