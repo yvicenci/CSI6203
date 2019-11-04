5ffcee732487a973e0fb292a24ee88b5409119fc
yvicenci <yvicenci@our.ecu.edu.au>
2019-11-04 07:52:53 +0000

    workshop-10: Almost finished task 5. just need to remove repeated disciplines

--- portfolio/week 10/AdvancedScraper.sh
+++ portfolio/week 10/AdvancedScraper.sh
@@ -5,24 +5,16 @@ IFS=
 # get web content
 ecu=$(curl -s https://www.ecu.edu.au/degrees/study-areas/science)
 
-echo "$ecu" | awk '
-BEGIN { startProcess=0 }
-{
-    regexStart="<section id=\"disciplines\".*"
-    if( $0 ~ regexStart ){
-        startProcess=1
-        regexScrape="<a href=\"https://www.ecu.edu.au/degrees/study-areas/science/.*\""
-        regexEnd="</section>"
-        print $0
-        while(startProcess==1){
-            if( $0 ~ regexScrape ){
-                print $0
-        }
-        else if( $0 ~ regexEnd ){
-            startProcess=0
+# use sed and awk to get disciplines in ECU Science
+echo "$ecu" | sed -rn "s@<a href=\"https://www.ecu.edu.au/degrees/study-areas/science.*\" title=\".*\">(.*)</a>@\1@p" | awk '
+    BEGIN { i=0; used[0]=""}
+    {
+        i+=1
+        if($0 in used)
+            next
+        else {
+            used[$0]=i
+            print $0
         }
     }
-    }
-
-}
-' #| sed -rn "s@<a href=\"https://www.ecu.edu.au/degrees/study-areas/science.*\" title=\".*\">(.*)</a>@\1@p"
+'
